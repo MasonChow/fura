77deb434e2f4ae14325e83b9b06d347ea493f9b1
zhoushunming <zhoushunming@joyy.com>
2023-02-15 13:31:37 +0800

    feat: 调整目录结构

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -4,11 +4,8 @@
  */
 
 import { cac } from 'cac';
-import Table from 'easy-table';
-import lodash from 'lodash';
-// import ora from 'ora';
 import { getConfig, getPackageJSON } from './helper/utils';
-import core from '../core';
+import unused, { Options as unUsedOptions } from './unused';
 
 const cli = cac('fura');
 
@@ -18,7 +15,7 @@ cli
     '指定入口文件分析项目内无效依赖项目, 默认拿当前执行目录',
   )
   .option('-c,-config <configPath>', '指定配置文件地址')
-  .option('-o,-output <outputPath>', '输出文件的地址')
+  // .option('-o,-output <outputPath>', '输出文件的地址')
   // .option(
   //   '-t,-type [type]',
   //   '指定输出的报告类型 log,html,excel,canvas,json(Default is json)',
@@ -26,103 +23,13 @@ cli
   .action(
     async (
       target: string,
-      { c, t = 'log' }: { c?: string; t: string; o: string },
+      { c, t }: { c?: string; t?: unUsedOptions['outputType'] },
     ) => {
       const config = getConfig(c);
       const alias = config('alias');
       const exclude = config('exclude');
       const deadCode = config('deadCode', true);
-      let outputContent: string;
-
-      const instance = await core({
-        cwd: target,
-        options: {
-          alias,
-          exclude,
-        },
-      });
-
-      // spinner.text = '分析依赖关系';
-
-      const { entry, include } = deadCode;
-
-      // 批量分析依赖结果
-      const results = await Promise.all(
-        entry.map((e) => {
-          return instance.getUnusedDeps(e, {
-            include,
-          });
-        }),
-      );
-
-      // 取多次的交集即可判断出真正没被使用的文件
-      const { files, npmPkgs } = results.reduce((prev, cur, idx) => {
-        if (idx === 0) {
-          return cur;
-        }
-
-        return {
-          files: lodash.intersectionBy(prev.files, cur.files, 'id'),
-          npmPkgs: lodash.intersectionBy(prev.npmPkgs, cur.npmPkgs, 'id'),
-        };
-      });
-
-      if (t === 'log') {
-        if (files.length) {
-          const fileTable = new Table();
-          files.forEach((file) => {
-            fileTable.cell('name', file.name);
-            fileTable.cell('path', file.path);
-            fileTable.cell('size', file.size);
-            fileTable.newRow();
-          });
-
-          fileTable.total('name', {
-            printer: (val) => {
-              return `count: ${val}`;
-            },
-            reduce: (acc, val, idx) => {
-              return idx + 1;
-            },
-          });
-
-          fileTable.total('size', {
-            printer: (val) => {
-              return `total: ${val}`;
-            },
-          });
-          outputContent = fileTable.toString();
-          console.info('----- 检测到存在未使用文件 ------');
-          console.info(outputContent);
-        } else {
-          console.info('🎉🎉🎉未检测到未被引用文件');
-        }
-
-        if (npmPkgs.length) {
-          const npmPkgsTable = new Table();
-          npmPkgs.forEach((npm) => {
-            npmPkgsTable.cell('name', npm.name);
-            npmPkgsTable.cell('version', npm.version);
-            npmPkgsTable.newRow();
-          });
-
-          npmPkgsTable.total('name', {
-            printer: (val) => {
-              return `count: ${val}`;
-            },
-            reduce: (acc, val, idx) => {
-              return idx + 1;
-            },
-          });
-          outputContent = npmPkgsTable.toString();
-          console.info(
-            '----- 检测到存在未使用的npm包(仅检测dependencies) ------',
-          );
-          console.info(outputContent);
-        } else {
-          console.info('🎉🎉🎉未检测到未被引用npm包');
-        }
-      }
+      await unused(target, deadCode, { alias, exclude, outputType: t });
       process.exit(0);
     },
   );
---
+++ src/bin/unused/index.ts
@@ -0,0 +1,58 @@
+/**
+ * @module 分析未使用文件
+ */
+import lodash from 'lodash';
+// import ora from 'ora';
+import { Config } from '../helper/type';
+import core from '../../core';
+
+import { printConsole } from './output';
+
+export type Options = Pick<Config, 'alias' | 'exclude'> & {
+  outputType?: 'console' | 'html' | 'json' | 'txt';
+};
+
+async function unused(
+  target: string,
+  deadCode: Required<Config>['deadCode'],
+  options: Options,
+) {
+  const { alias, exclude, outputType = 'console' } = options;
+  const instance = await core({
+    cwd: target,
+    options: {
+      alias,
+      exclude,
+    },
+  });
+
+  const { entry, include } = deadCode;
+  // 批量分析依赖结果
+  const results = await Promise.all(
+    entry.map((e) => {
+      return instance.getUnusedDeps(e, {
+        include,
+      });
+    }),
+  );
+  // 取多次的交集即可判断出真正没被使用的文件
+  const { files, npmPkgs } = results.reduce((prev, cur, idx) => {
+    if (idx === 0) {
+      return cur;
+    }
+    return {
+      files: lodash.intersectionBy(prev.files, cur.files, 'id'),
+      npmPkgs: lodash.intersectionBy(prev.npmPkgs, cur.npmPkgs, 'id'),
+    };
+  });
+
+  switch (outputType) {
+    case 'console':
+      printConsole(files, npmPkgs);
+      break;
+    default:
+      printConsole(files, npmPkgs);
+  }
+}
+
+export default unused;
