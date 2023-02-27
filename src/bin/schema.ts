06a4a196183fcb29f95972b8cf5ac51eae4c54c0
zhoushunming <zhoushunming@joyy.com>
2023-02-27 13:55:43 +0800

    feat: 封装方法

--- src/bin/schema.ts
+++ src/bin/schema.ts
@@ -13,9 +13,7 @@ import {
   conversionOriginUrl,
   flowChats,
 } from '../helper/mermaid';
-import { isJsOrTsFileType } from '../helper/utils';
 import diskCache from '../helper/diskCache';
-import { GetMapValue } from '../typings/utils';
 
 async function schema(
   target: string,
@@ -29,126 +27,53 @@ async function schema(
   });
   spinner.text = '解析文件关系';
 
-  const results = await Promise.all(
-    entry.map((entryFile) =>
-      instance.analysis.getFlattenFileFullRelation(entryFile),
-    ),
+  const { infoMap, relations } = await instance.getRelationFromFileComment(
+    entry,
   );
 
-  // 把多个map合成一个
-  const infoMap = results
-    .map((e) => e.infoMap)
-    .reduce((pre, cur) => {
-      [...cur.keys()].forEach((key) => {
-        const preItem = pre.get(key);
-        const curItem = cur.get(key)!;
-        // 避免合并map的过程中覆盖了isEntry字段
-        if (preItem && preItem.isEntry) {
-          curItem.isEntry = true;
-        }
-        pre.set(key, curItem);
-      });
-
-      return pre;
-    }, new Map<number, GetMapValue<Awaited<ReturnType<typeof instance.analysis.getFlattenFileFullRelation>>['infoMap']>>());
-
-  // 把多个relations合成一个
-  const relations = results.map((e) => e.relations).flat();
-
-  spinner.text = '分析生成结果';
+  spinner.text = '构建关系链路';
 
   const params: flowChats.CreateFlowchartsData = {
     links: [],
     itemMap: {},
   };
 
-  const simpleRelationMap = new Map<number, number[]>();
-  const simpleRelations = new Set<`${number}-${number}`>();
-  const lock = new Set<`${number}-${number}`>();
-
   relations.forEach((rel) => {
-    const fromData = infoMap.get(rel.from);
-    const toData = infoMap.get(rel.to);
+    const { from, to } = rel;
+    const fromData = infoMap.get(from);
+    const toData = infoMap.get(to);
 
-    // 过滤掉非js/ts文件
-    if (
-      !fromData ||
-      !isJsOrTsFileType(fromData.type) ||
-      !toData ||
-      !isJsOrTsFileType(toData.type)
-    ) {
-      return;
-    }
+    params.itemMap[from] = {
+      name: fromData?.attr?.name || toData?.name || 'unknown',
+      type: fromData?.isEntry ? 'circle' : undefined,
+    };
 
-    const fromId = fromData.id;
-    const toId = toData.id;
+    params.itemMap[to] = {
+      name: toData?.attr?.name || toData?.name || 'unknown',
+      type: toData?.isEntry ? 'circle' : undefined,
+    };
 
-    simpleRelationMap.set(fromId, [
-      ...(simpleRelationMap.get(fromId) || []),
-      toId,
-    ]);
+    params.links.push([String(from), String(to)]);
   });
 
-  function getAvailableNextIds(nextId: number): number[] {
-    const info = infoMap.get(nextId)!;
-
-    if (info.attr?.name) {
-      return [nextId];
-    }
-
-    const nextRelation = simpleRelationMap.get(nextId);
-
-    if (nextRelation) {
-      return nextRelation
-        .map((nextRelationId) => {
-          const lockKey = `${nextId}-${nextRelationId}` as const;
-          if (lock.has(lockKey)) {
-            return [];
-          }
-          lock.add(lockKey);
-          return getAvailableNextIds(nextRelationId);
-        })
-        .flat();
-    }
-
-    return [];
-  }
-
-  [...simpleRelationMap.keys()].forEach((fromId) => {
-    const fromData = infoMap.get(fromId)!;
-    const nextIds = simpleRelationMap.get(fromId)!;
-
-    if (fromData.attr?.name) {
-      params.itemMap[fromId] = {
-        name: fromData.attr.name,
-        type: fromData.isEntry ? 'circle' : undefined,
-      };
-
-      const simpleRelation = nextIds.map(getAvailableNextIds).flat();
-
-      simpleRelation.forEach((nextId) => {
-        const nextData = infoMap.get(nextId)!;
-        const uniqRelation = `${fromId}-${nextId}` as const;
-        params.itemMap[nextId] = {
-          name: nextData.attr?.name || nextData.name,
-        };
-
-        if (!simpleRelations.has(uniqRelation)) {
-          simpleRelations.add(uniqRelation);
-          params.links.push([String(fromId), String(nextId)]);
-        }
-      });
-    }
-  });
+  spinner.text = '生成图例';
 
   const flowChatsContent = flowChats.createFlowcharts(params);
 
   // const svgFile = await conversionToMedia(flowChatsContent, 'svg');
   const url = conversionOriginUrl(flowChatsContent);
 
-  diskCache.writeFileSync('./comment-relation.mmd', String(flowChatsContent));
+  spinner.text = '写入本地缓存';
+
+  const cachePath = diskCache.writeFileSync(
+    './comment-relation.mmd',
+    String(flowChatsContent),
+  );
+
   spinner.stop();
+
   console.info('生成结果:', url);
+  console.info('结果mermaid内容:', cachePath);
 }
 
 export default schema;
--- src/core/index.ts
+++ src/core/index.ts
@@ -2,7 +2,10 @@
  * @name 集成封装api方法
  */
 
+import lodash from 'lodash';
 import Analysis from '../analysis';
+import { GetMapValue } from '../typings/utils';
+import logger from '../helper/logger';
 
 export interface Config {
   // 分析入口目录 默认 .
@@ -39,6 +42,116 @@ export async function main(config: Config) {
         analysis.getProjectFiles(),
       ]);
       return { projectTree: tree, fileMap, dirMap };
+    },
+    /**
+     * 基于注释获取依赖关系
+     *
+     * @param entryFilePaths 分析的文件入口相对路径，相对于cwd
+     * @returns infoMap 对应id的Map数据
+     * @returns relations 对应的关系链
+     */
+    async getRelationFromFileComment(entryFilePaths: string[]) {
+      const results = await Promise.all(
+        entryFilePaths.map((entryFile) =>
+          analysis.getFlattenFileFullRelation(entryFile),
+        ),
+      );
+
+      // 把多个map合成一个
+      const infoMap = results
+        .map((e) => e.infoMap)
+        .reduce((pre, cur) => {
+          [...cur.keys()].forEach((key) => {
+            const preItem = pre.get(key);
+            const curItem = cur.get(key)!;
+            // 避免合并map的过程中覆盖了isEntry字段
+            if (preItem && preItem.isEntry) {
+              curItem.isEntry = true;
+            }
+            pre.set(key, curItem);
+          });
+
+          return pre;
+        }, new Map<number, GetMapValue<Awaited<ReturnType<typeof analysis.getFlattenFileFullRelation>>['infoMap']>>());
+
+      // 把多个relations合成一个，并去重重复的
+      const relations = lodash.uniqWith(
+        results.map((e) => e.relations).flat(),
+        lodash.isEqual,
+      );
+      const simpleRelations: Omit<(typeof relations)[0], 'type'>[] = [];
+
+      const simpleRelationMap = new Map<number, number[]>();
+      const simpleRelationsSet = new Set<`${number}-${number}`>();
+      const lock = new Set<`${number}-${number}`>();
+
+      relations.forEach((rel) => {
+        const fromData = infoMap.get(rel.from);
+        const toData = infoMap.get(rel.to);
+
+        if (!fromData || !toData) {
+          logger.error('缺失对应详情，跳过:');
+          logger.error(`fromDataId=${rel.from},fromData:`, fromData);
+          logger.error(`toDataId=${rel.to},toData:`, toData);
+          return;
+        }
+
+        const fromId = fromData.id;
+        const toId = toData.id;
+
+        simpleRelationMap.set(fromId, [
+          ...(simpleRelationMap.get(fromId) || []),
+          toId,
+        ]);
+      });
+
+      function getAvailableNextIds(nextId: number): number[] {
+        const info = infoMap.get(nextId)!;
+
+        if (info.attr?.name) {
+          return [nextId];
+        }
+
+        const nextRelation = simpleRelationMap.get(nextId);
+
+        if (nextRelation) {
+          return nextRelation
+            .map((nextRelationId) => {
+              const lockKey = `${nextId}-${nextRelationId}` as const;
+              if (lock.has(lockKey)) {
+                return [];
+              }
+              lock.add(lockKey);
+              return getAvailableNextIds(nextRelationId);
+            })
+            .flat();
+        }
+
+        return [];
+      }
+
+      [...simpleRelationMap.keys()].forEach((fromId) => {
+        const fromData = infoMap.get(fromId)!;
+        const nextIds = simpleRelationMap.get(fromId)!;
+
+        if (fromData.attr?.name) {
+          const simpleRelation = nextIds.map(getAvailableNextIds).flat();
+
+          simpleRelation.forEach((nextId) => {
+            const uniqRelation = `${fromId}-${nextId}` as const;
+
+            if (!simpleRelationsSet.has(uniqRelation)) {
+              simpleRelationsSet.add(uniqRelation);
+              simpleRelations.push({ from: fromId, to: nextId });
+            }
+          });
+        }
+      });
+
+      return {
+        infoMap,
+        relations: simpleRelations,
+      };
     },
     /** 分析sdk示例 */
     analysis,
