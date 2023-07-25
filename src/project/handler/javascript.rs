06d51136ef2f04dcaa90c131395146cfc7fb2db6
zhoushunming <zhoushunming@joyy.com>
2023-07-25 18:46:34 +0800

    feat: update

--- src/project/handler/javascript.rs
+++ src/project/handler/javascript.rs
@@ -1,6 +1,7 @@
 use crate::database::sqlite;
 use crate::parser;
 use std::collections::HashMap;
+use std::path::PathBuf;
 
 const EXTENSIONS: [&str; 6] = ["js", "jsx", "ts", "tsx", "cjs", "mjs"];
 
@@ -34,20 +35,41 @@ fn query_js_files() -> Result<HashMap<String, u64>, String> {
   Ok(js_file_map)
 }
 
+/// 查询项目内所有 js 文件
+fn query_npm_pkgs() -> Result<HashMap<String, u64>, String> {
+  let mut npm_pkg_map: HashMap<String, u64> = HashMap::new();
+
+  let conn = sqlite::get_db().expect("获取 DB 失败");
+
+  let mut stmt: sqlite::Statement = conn
+    .prepare(
+      r#"
+      SELECT
+        id,
+        name
+      FROM
+        "npm_pkg";
+      "#,
+    )
+    .expect("查询失败");
+
+  let result = stmt.query_map([], |row| Ok((row.get(0).unwrap(), row.get(1).unwrap())));
+
+  for item in result.unwrap() {
+    let (id, name) = item.unwrap();
+    npm_pkg_map.insert(name, id);
+  }
+
+  Ok(npm_pkg_map)
+}
+
+#[derive(Debug)]
 pub struct File {
   pub path: String,
   pub imports: HashMap<String, Vec<String>>,
 }
 
-impl File {
-  pub fn auto_complete_import_path(
-    &self,
-    base_path: &String,
-    alias: &Option<HashMap<String, String>>,
-  ) {
-  }
-}
-
+#[derive(Debug)]
 pub struct ProjectJavascriptFile {
   pub base_path: String,
   pub alias: Option<HashMap<String, String>>,
@@ -84,47 +106,84 @@ impl ProjectJavascriptFile {
   // './a' -> './a.(js|jsx|ts|tsx|cjs|mjs)'
   // 'antd' -> package.json -> dependencies -> antd
   // 'antd/lib/button' -> package.json -> dependencies -> antd
-  pub fn auto_complete_import_path(&self) {
+  pub fn auto_complete_import_path(&self) -> Self {
     let files = &self.files;
+    let npm_pkgs = query_npm_pkgs().unwrap();
+    let mut result_files: HashMap<String, File> = HashMap::new();
 
-    for (key, value) in files {}
-  }
-}
+    for (file_path, file) in files {
+      let file_imports = &file.imports;
+      let file_real_path = PathBuf::from(&file_path);
+      let mut real_imports: HashMap<String, Vec<String>> = HashMap::new();
 
-// pub fn auto_complete_import_path(project_file: &ProjectJavascriptFile) -> ProjectJavascriptFile {
-// let mut result: String = import_path.to_string();
+      for (import_path, import_modules) in file_imports {
+        // './a/b' -> './b/index.(js|jsx|ts|tsx|cjs|mjs)'
+        // './a' -> './a.(js|jsx|ts|tsx|cjs|mjs)'
+        let mut deps_path: String = import_path.to_string();
 
-// // 1. 判断是否是直接是文件后缀 .js .jsx .ts .tsx .cjs .mjs
-// for ext in EXTENSIONS.iter() {
-//   if result.ends_with(ext) {
-//     return result;
-//   }
-// }
+        // 有传入 alias，则进行匹配替换
+        match &self.alias {
+          Some(alias) => {
+            for (key, value) in alias {
+              if deps_path.starts_with(key) {
+                deps_path = deps_path.replace(key, value);
+              }
+            }
+          }
+          None => {}
+        }
 
-// match alias {
-//   Some(alias) => {
-//     for (key, value) in alias {
-//       if result.starts_with(&key) {
-//         result = result.replace(&key, &value);
-//       }
-//     }
-//   }
-//   None => {}
-// }
+        let mut deps_real_path = PathBuf::from(&deps_path);
 
-// return result;
+        // 如果是相对路径则进行拼接
+        if deps_real_path.is_relative() {
+          deps_real_path = file_real_path.join(&deps_real_path);
+        }
 
-// 2. 判断是否是相对路径
-// }
+        // 匹配出真实的依赖的文件路径
+        for ext in EXTENSIONS {
+          let mut deps_real_path_with_ext = deps_real_path.clone();
+          deps_real_path_with_ext.set_extension(ext);
 
-/// 分析项目内所有 js 文件
-pub fn analyze_all(base_path: String, alias: Option<HashMap<String, String>>) {
-  let js_file_map = query_js_files().unwrap();
-  for (path, _) in js_file_map {
-    let result = parser::javascript::File::new(&path);
+          // 真实的依赖文件路径存在，则跳出循环
+          if files.contains_key(deps_real_path_with_ext.to_str().unwrap()) {
+            deps_real_path = deps_real_path_with_ext;
+            break;
+          }
+        }
 
-    result.code_parser.imports.iter().for_each(|item| {
-      println!("{} => {:?}", path, item);
-    });
+        match deps_real_path.extension() {
+          Some(_) => {}
+          None => {
+            // 如果没有后缀，则尝试匹配 package.json 的 dependencies
+            for pkg in npm_pkgs.keys() {
+              if deps_real_path.starts_with(pkg) {
+                deps_real_path = PathBuf::from(pkg);
+                break;
+              }
+            }
+          }
+        }
+
+        real_imports.insert(
+          deps_real_path.to_str().unwrap().to_string(),
+          import_modules.clone(),
+        );
+      }
+
+      result_files.insert(
+        file_path.to_string(),
+        File {
+          path: file_path.to_string(),
+          imports: real_imports,
+        },
+      );
+    }
+
+    ProjectJavascriptFile {
+      base_path: self.base_path.clone(),
+      alias: self.alias.clone(),
+      files: result_files,
+    }
   }
 }
--- src/project/mod.rs
+++ src/project/mod.rs
@@ -21,6 +21,9 @@ pub fn init_project_data(root_path: &str, exclude_paths: Option<Vec<&str>>) {
     block_on(init_base::insert_package_json_data(&package_json));
   }
 
-  let _javascript_file =
+  let javascript_file =
     handler::javascript::ProjectJavascriptFile::new(root_path.to_string(), None);
+  let with_auto_paths_file = javascript_file.auto_complete_import_path();
+
+  println!("init project data success, {:?}", with_auto_paths_file);
 }
